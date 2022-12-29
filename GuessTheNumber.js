b1222bebb7c470d82f5b3c808ec17566bc6b6b24
Alexandru-Daniel Popescu <68361456+adpopescu338@users.noreply.github.com>
2022-12-29 21:22:00 +0200

    Update GuessTheNumber.js
    Nice one. Some tips here

--- GuessTheNumber.js
+++ GuessTheNumber.js
@@ -1,8 +1,7 @@
 //Ghiceste numarul de la 0 la 10
 //numarul ales este 7
 
-const guessTheno;
-guesstheno = prompt("Guess the number between 0 and 10 ");
+const guessTheno = prompt("Guess the number between 0 and 10 "); // when using const, you must always initialise it.
 console.log("Yor number is" + guesstheno);
 if(guessTheno ==7){
     console.log("Congratulation,you guessed the number" + guessTheno);
@@ -11,3 +10,21 @@ console.log("Your number is to low");
 }else(guessTheno>7){
     console.log("Yor numer is to high");
 }
+/*
+This will only run once. To make it run infintly, you can convert it to a recursive function.
+A recursive function is a function that calls itself:
+function guessTheNo (){
+    const nr = prompt("Guess the number between 0 and 10 ");
+    alert("Yor number is" + guesstheno);
+    if(guessTheno ==7){
+        alert("Congratulation,you guessed the number" + guessTheno);
+    }else if(guessTheno<7){
+        alert("Your number is to low");
+    }else(guessTheno>7){
+        alert("Yor numer is to high");
+    }
+    
+    // now, to make all this happen again, you call the function again from within
+    guessTheno()
+}
+*/
